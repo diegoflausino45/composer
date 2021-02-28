549d53de4515c850b26faf43405cd66fddab2efd
diegoflausino45 <diegocflausino@gmail.com>
2021-02-28 20:33:13 -0300

    Commit do ep1

---
+++ Ep1/index.php
@@ -0,0 +1,4 @@
+<?php
+require __DIR__."/vendor/autoload.php";
+
+echo "<h1> Olá mundo! Fazer acuntecer, PHP!</h1>";
---
+++ Ep1/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'diegoflausino45/phptips001',
+  ),
+  'versions' => 
+  array (
+    'diegoflausino45/phptips001' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.0.7',
+      'version' => '6.0.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0c41a36d4508d470e376498c1c0c527aa36a2d59',
+    ),
+  ),
+);
